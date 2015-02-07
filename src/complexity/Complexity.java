134e736c4deff632822ee0e5052dbfe2c2843f8f
20 <20@I20>
2015-02-07 09:40:20 -0500

    http://www.tutorialspoint.com/java/java_decision_making.htm

---
+++ src/complexity/Complexity.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package complexity;
+
+/**
+ *
+ * Walter Arboleda
+ * http://plugins.netbeans.org/plugin/9494/simple-code-metrics
+ * http://www.tutorialspoint.com/java/java_decision_making.htm
+ */
+public class Complexity {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+    int x = 30;
+
+      if( x < 20 ){
+         System.out.println("if true");
+      }else{
+         System.out.println("if false");
+      }
+   }
+    
+}
